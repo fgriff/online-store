a2f9f9fe5a80df493eb931a6bebec075b89e913d
fgriff <anglirum@gmail.com>
2022-12-20 16:07:09 +0300

    refactor: remove redundant classnames function

--- online-store/src/App.tsx
+++ online-store/src/App.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import styles from './App.module.scss';
-import classnames from 'classnames';
 
 const App = () => {
-  return <h1 className={classnames(styles.app)}>Hello world</h1>;
+  return <h1 className={styles.app}>Hello world</h1>;
 };
 
 export default App;
