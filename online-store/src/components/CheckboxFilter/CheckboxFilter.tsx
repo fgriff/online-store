4edf637c954544f16e164561f7a991c7068ed3aa
fgriff <anglirum@gmail.com>
2022-12-25 18:45:20 +0300

    refactor: move map function to return

--- online-store/src/components/CheckboxFilter/CheckboxFilter.tsx
+++ online-store/src/components/CheckboxFilter/CheckboxFilter.tsx
@@ -6,19 +6,16 @@ import styles from './CheckboxFilter.module.scss';
 const CheckboxFilter: FC<ICheckboxFiltersData> = (props) => {
   const { title, data } = props;
 
-  const filterItems = data.map((f) => (
-    <CheckboxItem
-      key={f.id}
-      {...f}
-    />
-  ));
-
   return (
     <div>
       <h3
         className={styles.filterTitle}
       >{`${title[0].toUpperCase()}${title.slice(1)}`}</h3>
-      <ul>{filterItems}</ul>
+      <ul>
+        {data.map((f) => (
+          <CheckboxItem {...f} />
+        ))}
+      </ul>
     </div>
   );
 };
