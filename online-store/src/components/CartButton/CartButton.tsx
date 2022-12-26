ecfbd2fddaf2a7d9f8f8b599ea8b62408565e108
fgriff <anglirum@gmail.com>
2022-12-26 16:42:06 +0300

    refactor: remove comments

--- online-store/src/components/CartButton/CartButton.tsx
+++ online-store/src/components/CartButton/CartButton.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import styles from './CartButton.module.scss';
 import logoIcon from '../../assets/img/png/cart.png';
-// import { NavLink } from 'react-router-dom';
 
 const CartButton = () => {
   return (
-    // TODO: после настройки роутинга заменить div на <NavLink to="/cart"></NavLink>
     <div className={styles.cartButton}>
       <img
         className={styles.cartImage}
--- online-store/src/components/Logo/Logo.tsx
+++ online-store/src/components/Logo/Logo.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import styles from './Logo.module.scss';
 import logoIcon from '../../assets/img/png/logo.png';
-// import { NavLink } from 'react-router-dom';
 
 const Logo = () => {
   return (
-    // TODO: после настройки роутинга заменить div на <NavLink to="/"></NavLink>
     <div className={styles.logo}>
       <img
         className={styles.logoImage}
