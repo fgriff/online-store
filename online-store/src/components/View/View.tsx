da89adf960703f168677710c5b51f9a766f04e55
fgriff <anglirum@gmail.com>
2022-12-26 22:44:57 +0300

    feat: implement View component

---
+++ online-store/src/components/View/View.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styles from './View.module.scss';
+import ViewListIcon from '@mui/icons-material/ViewList';
+import ViewModuleIcon from '@mui/icons-material/ViewModule';
+
+const View = () => {
+  return (
+    <div className={styles.view}>
+      <label className={styles.view__label}>
+        <input
+          className={styles.view__input}
+          type="radio"
+          name="view"
+        />
+        <span className={styles.view__icon}>
+          <ViewListIcon sx={{ fontSize: 30 }} />
+        </span>
+      </label>
+      <label className={styles.view__label}>
+        <input
+          className={styles.view__input}
+          type="radio"
+          name="view"
+          checked
+        />
+        <span className={styles.view__icon}>
+          <ViewModuleIcon sx={{ fontSize: 30 }} />
+        </span>
+      </label>
+    </div>
+  );
+};
+
+export default View;
