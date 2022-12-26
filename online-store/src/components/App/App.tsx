5e9dfafb0ef7eea2955cd11226045a2ef4bf8c1e
fgriff <anglirum@gmail.com>
2022-12-26 22:54:03 +0300

    feat: add simple routing for goods page

--- online-store/src/components/App/App.tsx
+++ online-store/src/components/App/App.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import GoodsPage from '../../pages/GoodsPage/GoodsPage';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
 import styles from './App.module.scss';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
-  return <div className={styles.app}></div>;
+  return (
+    <div className={styles.app}>
+      <Header />
+      <main className={styles.main}>
+        <Routes>
+          <Route
+            path="/"
+            element={<GoodsPage />}
+          />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
--- online-store/src/index.tsx
+++ online-store/src/index.tsx
@@ -2,12 +2,17 @@ import './index.scss';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
+import { BrowserRouter } from 'react-router-dom';
 
 const rootElement = document.getElementById('root');
 
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
-  root.render(<App />);
+  root.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  );
 } else {
   throw new Error('Root element not found');
 }
