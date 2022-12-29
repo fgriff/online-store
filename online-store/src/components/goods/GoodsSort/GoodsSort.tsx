a35fec2640dd35ca209fedb6351c00014e1c04f5
fgriff <anglirum@gmail.com>
2022-12-29 22:55:56 +0300

    feat: add actions for sort and search

--- online-store/src/components/goods/GoodsSort/GoodsSort.tsx
+++ online-store/src/components/goods/GoodsSort/GoodsSort.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useTypedDispatch, useTypedSelector } from '../../../redux/hooks';
+import { setSortType } from '../../../redux/slices/filtersSlice';
 import styles from './GoodsSort.scss';
 
 const GoodsSort = () => {
+  const dispatch = useTypedDispatch();
+
+  useEffect(() => {
+    dispatch(setSortType({ sortType: 'priceASC' }));
+  }, []);
+
+  const sortType = useTypedSelector((state) => state.filters.sortType);
+
+  const onChangeHandler = (value: string) => {
+    dispatch(setSortType({ sortType: value }));
+  };
+
   return (
     <div className={styles.sorting}>
-      <select className={styles.sorting__select}>
+      <select
+        className={styles.sorting__select}
+        value={sortType}
+        onChange={(e) => onChangeHandler(e.target.value)}
+      >
         <option value="priceASC">Price (asc)</option>
         <option value="priceDESC">Price (desc)</option>
         <option value="brandASC">Brand (asc)</option>
