65df166aeca4f18c2d9e6e77528ba873da1e79e1
IvanBusygin <bi-25@yandex.ru>
2023-01-08 10:49:24 +0400

    refactor: remove managed inputs

--- online-store/src/components/basket/FormProductRegistration/FormProductRegistration.tsx
+++ online-store/src/components/basket/FormProductRegistration/FormProductRegistration.tsx
@@ -1,15 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import style from './FormProductRegistration.scss';
 import { SubmitHandler, useForm } from 'react-hook-form';
-import { isValidPhoneNumber } from 'react-phone-number-input';
 import { useNavigate } from 'react-router-dom';
 
-import logoCard from '../../../assets/img/png/icon-card.png';
-import logoAExpress from '../../../assets/img/png/icon-AExpress.png';
-import logoMir from '../../../assets/img/png/icon-mir.png';
-import logoMastercard from '../../../assets/img/png/logo-mastercard.png';
-import logoUnionPay from '../../../assets/img/png/logo-unionPay.png';
-import logoVisa from '../../../assets/img/png/logo-visa.png';
+import logoCardDefault from '../../../assets/img/png/icon-card.png';
+import logoCardAExpress from '../../../assets/img/png/icon-AExpress.png';
+import logoCardMir from '../../../assets/img/png/icon-mir.png';
+import logoCardMastercard from '../../../assets/img/png/logo-mastercard.png';
+import logoCardUnionPay from '../../../assets/img/png/logo-unionPay.png';
+import logoCardVisa from '../../../assets/img/png/logo-visa.png';
 
 type IEventHandler = React.ChangeEvent<HTMLInputElement>;
 
@@ -23,112 +22,106 @@ interface IFormInputs {
   cvv: string;
 }
 
+enum ErrorMsg {
+  nameReq = 'Enter your name',
+  nameCor = 'Enter the name in the format: first-name last-name',
+  phoneReq = 'Enter your phone number',
+  phoneCor = 'Phone in the format +1 123 456 78 90',
+  addressReq = 'Enter your delivery address',
+  addressCor = 'Enter your 3-word address',
+  emailReq = 'Enter your email',
+  emailCor = 'Enter the correct email address',
+  cardNumber = 'Enter the correct card number',
+  cardNumberReq = 'Enter card number',
+  cardValidThru = 'Enter the correct Valid Thru',
+  cardValidThruReq = 'Enter Valid Thru',
+  cardCVV = 'Enter the correct CVV',
+  cardCVVReq = 'Enter cvv',
+}
+
+const regexpName = /[a-z|а-я]{3,}\s[a-z|а-я]{3,}/i;
+const regexpPhone =
+  /^(\+)((\d{2,3}) ?\d|\d)(([ -]?\d)|( ?(\d{2,3}) ?)){9,12}\d$/;
+const regexpAddress = /[a-z|а-я]{5,}\s[a-z|а-я]{5,}\s[a-z|а-я]{5,}/i;
+const regexpEmail =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const regexpCardNum = /\d{4}\s\d{4}\s\d{4}\s\d{4}/;
+const regexpCardValid = /(0[1-9]|1[0-2])\/[0-9]{2}/;
+
 function FormProductRegistration() {
   const {
     register,
-    formState: { errors, isValid },
+    formState: { errors },
     handleSubmit,
-    reset,
-  } = useForm<IFormInputs>({
-    mode: 'onChange',
-  });
-
-  const [cardNum, setCardNum] = useState<string>('');
-  const cardNumberHandler = ({ target }: IEventHandler) => {
-    if (target) {
-      const numStr = target.value
-        .replace(/[^\d]/g, '')
-        .replace(/\d{4}(?=.)/g, '$& ')
-        .slice(0, 19);
-      setCardNum(numStr);
-    }
-  };
-
-  const [cardValid, setCardValid] = useState<string>('');
-  const cardValidHandler = ({ target }: IEventHandler) => {
-    if (target) {
-      const numStr = target.value
-        .replace(/[^\d]/g, '')
-        .replace(/\d{2}(?=.)/g, '$&/')
-        .slice(0, 5);
-      setCardValid(numStr);
-    }
-  };
-
-  const [cardCVV, setCardCVV] = useState<string>('');
-  const cardCVVHandler = ({ target }: IEventHandler) => {
-    if (target) {
-      const numStr = target.value.replace(/[^\d]/g, '').slice(0, 3);
-      setCardCVV(numStr);
-    }
-  };
-
-  const [cardIcon, setCardIcon] = useState(logoCard);
-  useEffect(() => {
-    const firstLetter = cardNum[0];
-    let urlIcon;
-    switch (firstLetter) {
-      case '2':
-        urlIcon = logoMir;
-        break;
-      case '3':
-        urlIcon = logoAExpress;
-        break;
-      case '4':
-        urlIcon = logoVisa;
-        break;
-      case '5':
-        urlIcon = logoMastercard;
-        break;
-      case '6':
-        urlIcon = logoUnionPay;
-        break;
-      default:
-        urlIcon = logoCard;
-    }
-    setCardIcon(urlIcon);
-  }, [cardNum]);
+    setValue,
+  } = useForm<IFormInputs>({});
 
   const [sendOrder, setSendOrder] = useState<boolean>(false);
 
   const navigate = useNavigate();
 
-  const regexpEmail =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  const regexpName = /[a-z|а-я]{3,}\s[a-z|а-я]{3,}/i;
-  const regexpAddress = /[a-z|а-я]{5,}\s[a-z|а-я]{5,}\s[a-z|а-я]{5,}/i;
-  const regexpCardNum = /\d{4}\s\d{4}\s\d{4}\s\d{4}/;
-  const regexpValid = /(0[1-9]|1[0-2])\/[0-9]{2}/;
-
   const onSubmit: SubmitHandler<IFormInputs> = (data) => {
-    setCardNum('');
-    setCardCVV('');
-    setCardValid('');
-    reset();
+    console.log(data);
     setSendOrder(true);
 
     setTimeout(() => {
       setSendOrder(false);
       navigate('/');
-    }, 3000);
+    }, 5000);
   };
 
-  const enum ErrorMsg {
-    nameReq = 'Enter your name',
-    nameCor = 'Enter the name in the format: first-name last-name',
-    phoneReq = 'Enter your phone number',
-    phoneCor = 'Enter the correct phone number',
-    addressReq = 'Enter your delivery address',
-    addressCor = 'Enter your 3-word address',
-    emailReq = 'Enter your email',
-    emailCor = 'Enter the correct email address',
-    cardNumber = 'Enter the correct card number',
-    cardNumberReq = 'Enter card number',
-    cardValidThru = 'Enter the correct Valid Thru',
-    cardValidThruReq = 'Enter Valid Thru',
-    cardCVV = 'Enter the correct CVV',
-    cardCVVReq = 'Enter cvv',
-  }
+  const [cardIcon, setCardIcon] = useState(logoCardDefault);
+
+  const cardNumberHandler = ({ target }: IEventHandler) => {
+    const newValue = target.value
+      .replace(/[^\d]/g, '')
+      .replace(/\d{4}(?=.)/g, '$& ')
+      .slice(0, 19);
+
+    setValue('cardNumber', newValue);
+
+    const firstLetter = target.value[0];
+    switch (firstLetter) {
+      case '2':
+        setCardIcon(logoCardMir);
+        break;
+      case '3':
+        setCardIcon(logoCardAExpress);
+        break;
+      case '4':
+        setCardIcon(logoCardVisa);
+        break;
+      case '5':
+        setCardIcon(logoCardMastercard);
+        break;
+      case '6':
+        setCardIcon(logoCardUnionPay);
+        break;
+      default:
+        setCardIcon(logoCardDefault);
+    }
+  };
+
+  const cardValidHandler = ({ target }: IEventHandler) => {
+    let newValue = target.value
+      .replace(/[^\d]/g, '')
+      .replace(/\d{2}(?=.)/g, '$&/')
+      .slice(0, 5);
+
+    if (newValue.includes('/', 2)) {
+      const arrValue = newValue.split('/');
+      const monthValue: string = Number(arrValue[0]) > 12 ? '12' : arrValue[0];
+      newValue = `${monthValue}/${arrValue[1]}`;
+    } else {
+      if (Number(newValue) > 12) newValue = '12';
+    }
+    setValue('valid', newValue);
+  };
+
+  const cardCVVHandler = ({ target }: IEventHandler) => {
+    const newValue = target.value.replace(/[^\d]/g, '').slice(0, 3);
+    setValue('cvv', newValue);
+  };
 
   return (
     <div className={style.wrapper}>
@@ -157,8 +150,9 @@ function FormProductRegistration() {
             className={style.input}
             {...register('phone', {
               required: ErrorMsg.phoneReq,
-              validate: {
-                cor: (value) => isValidPhoneNumber(value) || ErrorMsg.phoneCor,
+              pattern: {
+                value: regexpPhone,
+                message: ErrorMsg.phoneCor,
               },
             })}
             placeholder={'Phone number'}
@@ -214,12 +208,15 @@ function FormProductRegistration() {
               className={style.input}
               {...register('cardNumber', {
                 required: ErrorMsg.cardNumberReq,
-                pattern: regexpCardNum,
+                minLength: 19,
                 maxLength: 19,
+                pattern: {
+                  value: regexpCardNum,
+                  message: ErrorMsg.cardNumber,
+                },
+                onChange: (e) => cardNumberHandler(e),
               })}
               placeholder={'Card number'}
-              value={cardNum}
-              onChange={cardNumberHandler}
             />
 
             <div>VALID</div>
@@ -227,23 +224,32 @@ function FormProductRegistration() {
               className={style.input}
               {...register('valid', {
                 required: ErrorMsg.cardValidThruReq,
-                pattern: regexpValid,
                 minLength: 5,
+                maxLength: 5,
+                pattern: {
+                  value: regexpCardValid,
+                  message: ErrorMsg.cardValidThru,
+                },
+                onChange: (e) => cardValidHandler(e),
               })}
               placeholder={'Valid Thru'}
-              value={cardValid}
-              onChange={cardValidHandler}
             />
             <div>CVV</div>
             <input
               className={style.input}
               {...register('cvv', {
                 required: ErrorMsg.cardCVVReq,
-                minLength: 3,
+                minLength: {
+                  value: 3,
+                  message: 'minLength 3',
+                },
+                maxLength: {
+                  value: 3,
+                  message: 'maxLength 3',
+                },
+                onChange: (e) => cardCVVHandler(e),
               })}
               placeholder={'CVV'}
-              value={cardCVV}
-              onChange={cardCVVHandler}
             />
           </div>
 
@@ -258,11 +264,12 @@ function FormProductRegistration() {
           </div>
         </div>
 
-        <input
+        <button
           className={style.submit}
           type="submit"
-          disabled={!isValid}
-        />
+        >
+          CONFIRM
+        </button>
         {sendOrder && (
           <p className={style.form__msg}>Your order has been sent!</p>
         )}
