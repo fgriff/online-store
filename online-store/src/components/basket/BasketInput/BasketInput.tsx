867d7668b33a50227290fe694615c56d0b93f8e2
IvanBusygin <bi-25@yandex.ru>
2023-01-10 04:28:05 +0400

    feat: add promo code

--- online-store/src/components/basket/BasketInput/BasketInput.tsx
+++ online-store/src/components/basket/BasketInput/BasketInput.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import style from './BasketInput.scss';
 
-const BasketInput = () => {
+type IEventHandler = React.ChangeEvent<HTMLInputElement>;
+
+interface IBasketInput {
+  onChange: (e: IEventHandler) => void;
+}
+
+const BasketInput = ({ onChange }: IBasketInput) => {
   return (
     <div className={style.input}>
       <input
+        onChange={(e) => onChange(e)}
         className={style.input__field}
         type="text"
         placeholder="Enter promo code"
--- online-store/src/components/basket/BasketSummary/BasketSummary.tsx
+++ online-store/src/components/basket/BasketSummary/BasketSummary.tsx
@@ -1,6 +1,7 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import style from './BasketSummary.scss';
 import BasketInput from '../BasketInput/BasketInput';
+// import classnames from 'classnames';
 
 interface IBasketSummary {
   totalProducts?: number;
@@ -8,8 +9,20 @@ interface IBasketSummary {
   onClick: (active: boolean) => void;
 }
 
+type IEventHandler = React.ChangeEvent<HTMLInputElement>;
+
 const BasketSummary: FC<IBasketSummary> = (props) => {
   const { totalProducts, totalSum = 0, onClick } = props;
+
+  const [isPromo, setIsPromo] = useState<boolean>(false);
+
+  const promos = ['rs', 'epm'];
+
+  const inputHandler = ({ target }: IEventHandler) => {
+    const promo = target.value.toLowerCase();
+    setIsPromo(promos.includes(promo));
+  };
+
   return (
     <div className={style.summary}>
       <h2 className={style.summary__title}>Summary</h2>
@@ -18,13 +31,23 @@ const BasketSummary: FC<IBasketSummary> = (props) => {
           Products:{' '}
           <span className={style.summary__colorText}>{totalProducts}</span>
         </div>
-        <div>
+        <div className={isPromo ? style.summary__promo : ''}>
           Total sum: €{' '}
           <span
             className={style.summary__colorText}
           >{`${totalSum.toLocaleString('en-US')}`}</span>
         </div>
-        <BasketInput />
+        {isPromo ? (
+          <div>
+            New sum: €{' '}
+            <span className={style.summary__colorText}>{`${(
+              totalSum * 0.9
+            ).toLocaleString('en-US')}`}</span>
+            <p className={style.summary__discount}>Discount 10%</p>
+          </div>
+        ) : null}
+        <BasketInput onChange={inputHandler} />
+        <p className={style.summary__test}>Promo for test: 'RS', 'EPM'</p>
         <button
           disabled={totalProducts === 0}
           className={style.summary__button}
