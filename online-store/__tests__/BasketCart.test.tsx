c8fc2db774447656794d7c1e31639c38523747cd
fgriff <anglirum@gmail.com>
2023-01-13 22:26:35 +0300

    refactor: remove Provider and BrowserRouter from BasketCart test

--- online-store/__tests__/BasketCart.test.tsx
+++ online-store/__tests__/BasketCart.test.tsx
@@ -4,9 +4,6 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import React from 'react';
-import { Provider } from 'react-redux';
-import { store } from '../src/redux/store';
-import { BrowserRouter } from 'react-router-dom';
 import BasketCart from '../src/components/basket/BasketCart/BasketCart';
 
 describe('BasketCart', () => {
@@ -30,15 +27,12 @@ describe('BasketCart', () => {
     };
 
     render(
-      <Provider store={store}>
-        <BasketCart
-          card={card}
-          item={1}
-          incQuantity={() => {}}
-          decQuantity={() => {}}
-        />
-      </Provider>,
-      { wrapper: BrowserRouter },
+      <BasketCart
+        card={card}
+        item={1}
+        incQuantity={() => {}}
+        decQuantity={() => {}}
+      />,
     );
 
     expect(screen.getByTestId('quantity')).toHaveTextContent('10');
