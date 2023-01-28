3b1273136614fca609978073802b3feb6edeec80
fgriff <anglirum@gmail.com>
2023-01-28 14:53:56 +0300

    fix: fix test for validation form

--- online-store/__tests__/FormProductRegistration.test.tsx
+++ online-store/__tests__/FormProductRegistration.test.tsx
@@ -2,7 +2,8 @@
  * @jest-environment jsdom
  */
 import React from 'react';
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
 import '@testing-library/jest-dom';
 import FormProductRegistration from '../src/components/basket/FormProductRegistration/FormProductRegistration';
 import { Provider } from 'react-redux';
@@ -25,7 +26,7 @@ describe('BasketForm', () => {
       /Phone number/i,
     ) as HTMLInputElement;
     const inputAddress = screen.getByPlaceholderText(
-      /Delivery addres/i,
+      /Delivery address/i,
     ) as HTMLInputElement;
     const inputEmail = screen.getByPlaceholderText(
       /E-mail/i,
@@ -47,7 +48,7 @@ describe('BasketForm', () => {
     expect(inputCVV).toBeInTheDocument();
   });
 
-  it('BasketForm validation (incorrect data)', async () => {
+  it('BasketForm validation (incorrect data)', () => {
     render(
       <Provider store={store}>
         <FormProductRegistration />,
@@ -62,7 +63,7 @@ describe('BasketForm', () => {
       /Phone number/i,
     ) as HTMLInputElement;
     const inputAddress = screen.getByPlaceholderText(
-      /Delivery addres/i,
+      /Delivery address/i,
     ) as HTMLInputElement;
     const inputEmail = screen.getByPlaceholderText(
       /E-mail/i,
@@ -77,38 +78,40 @@ describe('BasketForm', () => {
 
     const button = screen.getByRole('button');
 
-    fireEvent.change(inputName, { target: { value: 'Name Su' } });
-    fireEvent.change(inputPhone, { target: { value: '+12345678' } });
-    fireEvent.change(inputAddress, { target: { value: 'Region House' } });
-    fireEvent.change(inputEmail, { target: { value: 'qwerty@mail' } });
-    fireEvent.change(inputCardNum, { target: { value: '1234567890' } });
-    fireEvent.change(inputValid, { target: { value: '14' } });
-    fireEvent.change(inputCVV, { target: { value: '12' } });
+    expect(() => {
+      act(() => {
+        fireEvent.change(inputName, { target: { value: 'Name Su' } });
+        fireEvent.change(inputPhone, { target: { value: '+12345678' } });
+        fireEvent.change(inputAddress, { target: { value: 'Region House' } });
+        fireEvent.change(inputEmail, { target: { value: 'qwerty@mail' } });
+        fireEvent.change(inputCardNum, { target: { value: '1234567890' } });
+        fireEvent.change(inputValid, { target: { value: '14' } });
+        fireEvent.change(inputCVV, { target: { value: '12' } });
 
-    fireEvent.click(button);
+        fireEvent.click(button);
 
-    await waitFor(() => {
-      const nameError = screen.queryByText(/Enter the name in the format/i);
-      const phoneError = screen.queryByText(/Phone in the format/i);
-      const addressError = screen.queryByText(/Enter your 3-word address/i);
-      const emailError = screen.queryByText(/Enter the correct email address/i);
-      const cardNumberError = screen.queryByText(
-        /Enter the correct card number/i,
-      );
-      const validError = screen.queryByText(/Enter the correct Valid Thru/i);
-      const cvvError = screen.queryByText(/Enter the correct CVV/i);
+        const nameError = screen.getByText(/Enter the name in the format/i);
+        const phoneError = screen.getByText(/Phone in the format/i);
+        const addressError = screen.getByText(/Enter your 3-word address/i);
+        const emailError = screen.getByText(/Enter the correct email address/i);
+        const cardNumberError = screen.getByText(
+          /Enter the correct card number/i,
+        );
+        const validError = screen.getByText(/Enter the correct Valid Thru/i);
+        const cvvError = screen.getByText(/Enter the correct CVV/i);
 
-      expect(nameError).toBeInTheDocument();
-      expect(phoneError).toBeInTheDocument();
-      expect(addressError).toBeInTheDocument();
-      expect(emailError).toBeInTheDocument();
-      expect(cardNumberError).toBeInTheDocument();
-      expect(validError).toBeInTheDocument();
-      expect(cvvError).toBeInTheDocument();
+        expect(nameError).toBeInTheDocument();
+        expect(phoneError).toBeInTheDocument();
+        expect(addressError).toBeInTheDocument();
+        expect(emailError).toBeInTheDocument();
+        expect(cardNumberError).toBeInTheDocument();
+        expect(validError).toBeInTheDocument();
+        expect(cvvError).toBeInTheDocument();
+      });
     });
   });
 
-  it('BasketForm validation (correct data)', async () => {
+  it('BasketForm validation (correct data)', () => {
     render(
       <Provider store={store}>
         <FormProductRegistration />,
@@ -123,7 +126,7 @@ describe('BasketForm', () => {
       /Phone number/i,
     ) as HTMLInputElement;
     const inputAddress = screen.getByPlaceholderText(
-      /Delivery addres/i,
+      /Delivery address/i,
     ) as HTMLInputElement;
     const inputEmail = screen.getByPlaceholderText(
       /E-mail/i,
@@ -138,36 +141,40 @@ describe('BasketForm', () => {
 
     const button = screen.getByRole('button');
 
-    fireEvent.change(inputName, { target: { value: 'Name Surname' } });
-    fireEvent.change(inputPhone, { target: { value: '+123456789' } });
-    fireEvent.change(inputAddress, {
-      target: { value: 'Region Street House' },
-    });
-    fireEvent.change(inputEmail, { target: { value: 'qwerty@mail.ru' } });
-    fireEvent.change(inputCardNum, { target: { value: '1111222233334444' } });
-    fireEvent.change(inputValid, { target: { value: '12' } });
-    fireEvent.change(inputCVV, { target: { value: '111' } });
+    expect(() => {
+      act(() => {
+        fireEvent.change(inputName, { target: { value: 'Name Surname' } });
+        fireEvent.change(inputPhone, { target: { value: '+123456789' } });
+        fireEvent.change(inputAddress, {
+          target: { value: 'Region Street House' },
+        });
+        fireEvent.change(inputEmail, { target: { value: 'qwerty@mail.ru' } });
+        fireEvent.change(inputCardNum, {
+          target: { value: '1111222233334444' },
+        });
+        fireEvent.change(inputValid, { target: { value: '12' } });
+        fireEvent.change(inputCVV, { target: { value: '111' } });
 
-    fireEvent.click(button);
+        fireEvent.click(button);
 
-    await waitFor(() => {
-      const nameError = screen.queryByText(/Enter the name in the format/i);
-      const phoneError = screen.queryByText(/Phone in the format/i);
-      const addressError = screen.queryByText(/Enter your 3-word address/i);
-      const emailError = screen.queryByText(/Enter the correct email address/i);
-      const cardNumberError = screen.queryByText(
-        /Enter the correct card number/i,
-      );
-      const validError = screen.queryByText(/Enter the correct Valid Thru/i);
-      const cvvError = screen.queryByText(/Enter the correct CVV/i);
+        const nameError = screen.getByText(/Enter the name in the format/i);
+        const phoneError = screen.getByText(/Phone in the format/i);
+        const addressError = screen.getByText(/Enter your 3-word address/i);
+        const emailError = screen.getByText(/Enter the correct email address/i);
+        const cardNumberError = screen.getByText(
+          /Enter the correct card number/i,
+        );
+        const validError = screen.getByText(/Enter the correct Valid Thru/i);
+        const cvvError = screen.getByText(/Enter the correct CVV/i);
 
-      expect(nameError).not.toBeInTheDocument();
-      expect(phoneError).not.toBeInTheDocument();
-      expect(addressError).not.toBeInTheDocument();
-      expect(emailError).not.toBeInTheDocument();
-      expect(cardNumberError).not.toBeInTheDocument();
-      expect(validError).not.toBeInTheDocument();
-      expect(cvvError).not.toBeInTheDocument();
+        expect(nameError).not.toBeInTheDocument();
+        expect(phoneError).not.toBeInTheDocument();
+        expect(addressError).not.toBeInTheDocument();
+        expect(emailError).not.toBeInTheDocument();
+        expect(cardNumberError).not.toBeInTheDocument();
+        expect(validError).not.toBeInTheDocument();
+        expect(cvvError).not.toBeInTheDocument();
+      });
     });
   });
 });
